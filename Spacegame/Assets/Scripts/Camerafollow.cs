9e6b085856389510dd392d26aca14643435e19a6
Tim-Plassmann <94110746+Tim-Plassmann@users.noreply.github.com>
2023-11-25 22:17:32 +0100

    first person

---
+++ Spacegame/Assets/Scripts/Camerafollow.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class FollowShip : MonoBehaviour
+{
+    public Transform target; // The ship's transform
+    public Vector3 positionOffset = new Vector3(0f, 5f, -10f); // Offset from the ship's position
+    public Vector3 rotationOffset = new Vector3(20f, 0f, 0f); // Offset for camera rotation
+
+    void Update()
+    {
+        if (target != null)
+        {
+            // Update the camera position based on the ship's position and offset
+            transform.position = target.position + positionOffset;
+
+            // Apply an additional rotation offset to the camera
+            transform.rotation = Quaternion.Euler(rotationOffset);
+
+            // Rotate the camera to follow the ship's rotation
+            transform.Rotate(target.eulerAngles);
+        }
+    }
+}
