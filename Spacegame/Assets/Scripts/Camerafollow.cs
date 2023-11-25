0fde3b89fc0965fb9cdf2375ac6c2d1b78405312
Tim-Plassmann <94110746+Tim-Plassmann@users.noreply.github.com>
2023-11-25 22:18:55 +0100

    extra camera update

--- Spacegame/Assets/Scripts/Camerafollow.cs
+++ Spacegame/Assets/Scripts/Camerafollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FollowShip : MonoBehaviour
 {
     public Transform target; // The ship's transform
-    public Vector3 positionOffset = new Vector3(0f, 5f, -10f); // Offset from the ship's position
+    public Vector3 positionOffset = new Vector3(0f, 2f, 8f); // Offset from the ship's position
     public Vector3 rotationOffset = new Vector3(20f, 0f, 0f); // Offset for camera rotation
 
     void Update()
