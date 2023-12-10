081da2536f0b5f9a5c826332f29ecebe611c7c65
Tim-Plassmann <94110746+Tim-Plassmann@users.noreply.github.com>
2023-12-11 00:48:13 +0100

    shipmovement

--- Spacegame/Assets/Scripts/Movement/new scripts/ShipMovementInput.cs
+++ Spacegame/Assets/Scripts/Movement/new scripts/ShipMovementInput.cs
@@ -7,13 +7,13 @@ public class ShipMovementInput : MonoBehaviour
     [SerializeField] ShipInputManager.InputType inputType = ShipInputManager.InputType.HumanDesktop;
 
     public IMovementControls MovementControls { get; private set; }
-    // Start is called before the first frame update
+    
     void Start()
     {
         MovementControls = ShipInputManager.GetInputcontrols(inputType);
     }
 
-    // Update is called once per frame
+   
     void OnDestroy()
     {
         MovementControls = null;
